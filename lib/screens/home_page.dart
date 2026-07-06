254faed4a7c9ea35cee21627f98523424d2d6441
srpallab <pallab.alif@gmail.com>
2026-07-06 20:50:52 +0600

    Step-4

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,14 +1,32 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:testing_flutter_hooks/extensions/compact_map.dart';
 
 // Stream<String> getTime() => Stream.periodic(
 //   const Duration(seconds: 1),
 //   (_) => DateTime.now().toIso8601String(),
 // );
 
-const url = 'https://images.unsplash.com/photo-1544085311-11a028465b03';
+// const url = 'https://images.unsplash.com/photo-1544085311-11a028465b03';
+
+class CountDown extends ValueNotifier {
+  late StreamSubscription sub;
+
+  CountDown({required int from}) : super(from) {
+    sub = Stream.periodic(const Duration(seconds: 1), (v) => from - v)
+        .takeWhile((value) => value >= 0)
+        .listen((value) {
+          this.value = value;
+        });
+  }
+
+  @override
+  void dispose() {
+    sub.cancel();
+    super.dispose();
+  }
+}
 
 class HomePage extends HookWidget {
   const HomePage({super.key});
@@ -26,24 +44,28 @@ class HomePage extends HookWidget {
     //   });
     //   return null;
     // }, [controller]);
-    final future = useMemoized(
-      () => NetworkAssetBundle(Uri.parse(url))
-          .load(url)
-          .then((data) => data.buffer.asUint8List())
-          .then((data) => Image.memory(data)),
-    );
+    // final future = useMemoized(
+    //   () => NetworkAssetBundle(Uri.parse(url))
+    //       .load(url)
+    //       .then((data) => data.buffer.asUint8List())
+    //       .then((data) => Image.memory(data)),
+    // );
 
-    final snapshot = useFuture(future);
+    // final snapshot = useFuture(future);
+
+    final countDown = useMemoized(() => CountDown(from: 20));
+    final notifier = useListenable(countDown);
 
     return Scaffold(
       appBar: AppBar(title: Text("Home Page")),
-      body: Column(
-        children: [
-          snapshot.data,
-          // TextField(controller: controller),
-          // Text("You Typed ${text.value}"),
-        ].compactMap().toList(),
-      ),
+      body: Center(child: Text(notifier.value.toString())),
+      //Column(
+      // children: [
+      //   snapshot.data,
+      //   // TextField(controller: controller),
+      //   // Text("You Typed ${text.value}"),
+      // ].compactMap().toList(),
+      //),
     );
   }
 }
